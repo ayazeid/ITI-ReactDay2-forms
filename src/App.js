8c7045104fdfc841f0b3f6200afb37bc46886de4
ayazeid <aya.zeid95@gmail.com>
2022-01-05 10:55:01 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Nav from "./components/Nav";
 import LoginForm from "./forms/LoginForm";
 import RegisterForm from "./forms/RegisterForm";
 import ToDo from "./forms/ToDo";
@@ -7,9 +9,17 @@ import ToDo from "./forms/ToDo";
 function App() {
   return (
     <div className="App">
-      {/* <LoginForm /> */}
-      {/* <RegisterForm /> */}
-      <ToDo />
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={LoginForm} />
+          {/* <LoginForm /> */}
+          <Route exact path="/register" component={RegisterForm} />
+          {/* <RegisterForm /> */}
+          <Route exact path="/todo" component={ToDo} />
+          {/* <ToDo /> */}
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import { Link, NavLink } from "react-router-dom";
+
+function Nav() {
+  return (
+    <ButtonGroup
+      className="m-5"
+      variant="outlined"
+      aria-label="button group"
+      color="info"
+    >
+      <Button>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          Login
+        </Link>
+      </Button>
+      <Button>
+        <Link
+          to="/register"
+          style={{ color: "inherit", textDecoration: "none" }}
+        >
+          Register
+        </Link>
+      </Button>
+      <Button>
+        <Link to="/todo" style={{ color: "inherit", textDecoration: "none" }}>
+          ToDo
+        </Link>
+      </Button>
+    </ButtonGroup>
+  );
+}
+
+export default Nav;
